c5b4d51aabba0ea660f3b537baa5fcd2ba6e76b5
milhamfirdaus <muhamadilhamfirdaus@hotmail.com>
2020-10-19 16:17:32 +0700

    fix css

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<title>DTS CCL ITB - Task</title>
-		 <link href="./assets/styles.css" rel="stylesheet" type="text/css">
+		 <link href="assets/styles.css" rel="stylesheet" type="text/css">
 		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" type="text/css">
 		 <link rel="stylesheet" href=".https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
 		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
